c61f365b9e577cb2343fde237fed37ec7f084b6e
AW2019-acm00 <56448496+AW2019-acm00@users.noreply.github.com>
2020-05-22 13:01:11 +0200

    Add files via upload

---
+++ languages/en.php
@@ -0,0 +1,30 @@
+<?php
+  $lang = array(
+    "title" => "Home - Mallorca's Fantasy Paradise",
+    "lang" => "English",
+    "nav-home" => "Home",
+    "nav-housing" => "Housing",
+    "nav-mallorca" => "Why Mallorca?",
+    "nav-guides" => "Turistic guides",
+    "nav-about" => "About us",
+    "nav-about-us" => "Us",
+    "nav-faq" => "F . A . Q .",
+    "nav-terms" => "Terms and Conditions",
+    "nav-regiser" => "Regsiter",
+
+
+        "home-search" => "Search",
+
+        "home-h1" => "Looking for a holiday rental in Mallorca?",
+        "home-p1" => "Using our browser you can find the best houses and apartments to stay in all of Mallorca!",
+
+        "home-h2" => "We can offer",
+        "home-p2" => "{ From houses on the beachfront }",
+        "home-p3" => "{ even entire country estates for you }",
+
+
+    "nav-footer" => "This web page is a project of Vocational Training at the end of the Middle Degree of Microcomputer Systems of the IES Emili Darder institute. None of the offers, services and / or products are real.",
+
+  );
+
+ ?>
---
+++ languages/es.php
@@ -0,0 +1,30 @@
+<?php
+  $lang = array(
+    "title" => "Inicio - Mallorca's Fantasy Paradise",
+    "lang" => "Español",
+    "nav-home" => "Inicio",
+    "nav-housing" => "Alojamientos",
+    "nav-mallorca" => "¿Por qué Mallorca?",
+    "nav-guides" => "Guías turísticas",
+    "nav-about" => "Sobre nosotros",
+    "nav-about-us" => "Nosotros",
+    "nav-faq" => "F . A . Q .",
+    "nav-terms" => "Términos y condiciones",
+    "nav-regiser" => "Regsitrarse",
+
+    "home-search" => "Buscar",
+
+    "home-h1" => "¿Buscas un alquiler vacacional en Mallorca?",
+    "home-p1" => "Utilizando nuestro navegador puedes encontrar las mejores casas y apartamentos donde quedarte de toda Mallorca!",
+
+    "home-h2" => "Podemos ofrecerte",
+    "home-p2" => "{ Desde casas en primera linea de playa }",
+    "home-p3" => "{ hasta fincas de campo enteras para ti }",
+
+
+
+    "nav-footer" => "Esta página web es un proyecto de final de Grado Medio de Sistemas microinformáticos del instituto IES Emili Darder. Ninguna de las ofertas, servicios y/o productos son reales.",
+
+  );
+
+ ?>
